4d786b09d2978d89677ee2b9c1d827d9e589506a
MINH HOANG <quangminh02082006@gmail.com>
2025-10-01 13:46:15 +0300

    activity2-wednesday

--- backend-protected/controllers/userControllers.js
+++ backend-protected/controllers/userControllers.js
@@ -17,20 +17,20 @@ const signupUser = async (req, res) => {
     name,
     email,
     password,
-    phone_number,
-    gender,
-    date_of_birth,
-    membership_status,
+    role,
+    bio,
+    
   } = req.body;
   try {
     if (
       !name ||
       !email ||
       !password ||
-      !phone_number ||
-      !gender ||
-      !date_of_birth ||
-      !membership_status
+      !bio ||
+      !role 
+      
+
+      
     ) {
       res.status(400);
       throw new Error("Please add all fields");
@@ -52,10 +52,9 @@ const signupUser = async (req, res) => {
       name,
       email,
       password: hashedPassword,
-      phone_number,
-      gender,
-      date_of_birth,
-      membership_status,
+      role,
+      bio,
+      
     });
 
     if (user) {
